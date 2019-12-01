6ea69d9aacbec6e89b46e43d778735856dec2768
Jie Zhang <jzhang918@gmail.com>
2019-11-30 21:08:47 -0500

    adapt index.js for my AoC 2019

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-let year = 2017;
 let startDay = 1;
 let endDay = 25;
 const args = process.argv.slice(2);
 for(let arg of args) {
     const n = Number(arg);
-    if (n > 2000) {
-        year = n;
-    } 
-    else if (n > 0) {
+    if (n > 0) {
         startDay = n;
         endDay = n;
     }
@@ -20,35 +16,35 @@ const timed = fn => {
     const start = process.hrtime();
     const output = fn();
     const [secs,nanosecs] = process.hrtime(start);
-    const duration = secs*1000 + Math.floor(nanosecs/1000000);
-    return [output, duration];
+    const ms = Math.floor(nanosecs/1000000);
+    return [output, secs, ms];
 }
 
-const showTestResult = (year, day, part, expected, actual, duration) => {
-    const durationDesc = chalk.blue(` (${duration}ms)`);
+const showTestResult = (day, part, expected, actual, secs, ms) => {
+    const durationDesc = chalk.blue(` (${secs}s${ms}ms)`);
     if (actual === expected) {
-        console.log(chalk.green(`${year} day ${day} part ${part}: ${actual}`) + durationDesc);
+        console.log(chalk.green(`day ${day} part ${part}: ${actual}`) + durationDesc);
     }
     else {
-        console.log(chalk.red(`${year} day ${day} part ${part}: ${actual} - expected ${expected}` + durationDesc));
+        console.log(chalk.red(`day ${day} part ${part}: ${actual} - expected ${expected}` + durationDesc));
     }
 }
 
 for(let day = startDay; day <= endDay; day++) {
-    const path = `./${year}/${("0" + day).slice(-2)}`;
+    const path = `./${("0" + day).slice(-2)}`;
     if (!fs.existsSync(path)) {
-        console.log(chalk.red(`${year} day ${day} not found`));
-        break;
+        console.log(chalk.red(`day ${day} not found`));
+        continue;
     }
     const solver = require(path +`/solve`);
-    const text = fs.readFileSync(path + `/input.txt`)
+    const input = fs.readFileSync(path + `/input.txt`)
                     .toString()
                     .split('\n')
                     .map(s => s.replace(/\r$/, ''))
                     .filter(s => s.length > 0);
     for(let part of [1,2]) {
         const expected = solver.expected(part);
-        const [answer,duration] = timed(() => solver.solve(text, part));
-        showTestResult(year, day, part, expected, answer, duration);
+        const [answer,secs,ms] = timed(() => solver.solve(input, part));
+        showTestResult(day, part, expected, answer, secs, ms);
     }
 }
