413a38226b0827a751367c2bb7ae7a35fd6f08a2
Jie Zhang <jzhang918@gmail.com>
2019-12-05 08:18:46 -0500

    solution for Day 5 (part 2)

--- 05/solve.js
+++ 05/solve.js
@@ -19,12 +19,16 @@ function run(program, input) {
     const MULT = 2;
     const INPUT = 3;
     const OUTPUT = 4;
+    const JUMP_IF_TRUE = 5;
+    const JUMP_IF_FALSE = 6;
+    const LESS_THAN = 7;
+    const EQUALS = 8;
     const STOP = 99;
 
-    while (program[pc] !== STOP)
-    {
+    while (program[pc] !== STOP) {
         let opcode = program[pc] % 100;
         let a0, a1, a2;
+        let mode0, mode1;
         let result;
 
         switch (opcode) {
@@ -45,22 +49,73 @@ function run(program, input) {
 
             case ADD:
             case MULT:
-                a0 = program[pc + 1]; // op0
-                a1 = program[pc + 2]; // op1
-                a2 = program[pc + 3]; // result
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                a2 = program[pc + 3];
                 mode0 = Math.trunc(program[pc] / 100) % 10;
                 mode1 = Math.trunc(program[pc] / 1000) % 10;
 
                 if (opcode === ADD)
-                {
                     program[a2] = operand(program, a0, mode0) + operand(program, a1, mode1);
-                    pc += 4;
-                }
-                else if (opcode === MULT)
-                {
+                else
                     program[a2] = operand(program, a0, mode0) * operand(program, a1, mode1);
-                    pc += 4;
-                }
+                pc += 4;
+                break;
+
+            case JUMP_IF_TRUE:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) !== 0)
+                    pc = operand(program, a1, mode1);
+                else
+                    pc += 3;
+
+                break;
+
+            case JUMP_IF_FALSE:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) === 0)
+                    pc = operand(program, a1, mode1);
+                else
+                    pc += 3;
+
+                break;
+
+            case LESS_THAN:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                a2 = program[pc + 3];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) < operand(program, a1, mode1))
+                    program[a2] = 1;
+                else 
+                    program[a2] = 0;
+                pc += 4;
+
+                break;
+
+            case EQUALS:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                a2 = program[pc + 3];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) === operand(program, a1, mode1))
+                    program[a2] = 1;
+                else 
+                    program[a2] = 0;
+                pc += 4;
+
                 break;
 
             default:
@@ -74,13 +129,11 @@ function run(program, input) {
 }
 
 function solve(input, part) {
+    let program = input[0].split(',').map(x => Number(x));
     if (part === 1)
-    {
-        let program = input[0].split(',').map(x => Number(x));
         return run(program, [1]);
-    }
     else
-        return 0;
+        return run(program, [5]);
 }
 
 const expected = part => part === 1 ? 0 : 0;
