ba976815ae23bedf87b259ff004c0bac3fe02495
Jie Zhang <jzhang918@gmail.com>
2019-12-07 09:30:21 -0500

    solution for Day 7 (part 1)

---
+++ 07/solve.js
@@ -0,0 +1,198 @@
+function operand(program, value, mode)
+{
+    const POS_MODE = 0;
+    const IMM_MODE = 1;
+
+    if (mode === POS_MODE)
+        return program[value];
+    else if (mode === IMM_MODE)
+        return value;
+    else
+        console.log(`unknown mode ${mode}`);
+        return 0;
+}
+
+function run(program, input) {
+    let pc = 0;
+    let input_index = 0;
+    let output = [];
+    const ADD = 1;
+    const MULT = 2;
+    const INPUT = 3;
+    const OUTPUT = 4;
+    const JUMP_IF_TRUE = 5;
+    const JUMP_IF_FALSE = 6;
+    const LESS_THAN = 7;
+    const EQUALS = 8;
+    const STOP = 99;
+
+outer:
+    while (program[pc] !== STOP) {
+        let opcode = program[pc] % 100;
+        let a0, a1, a2;
+        let mode0, mode1;
+        let result;
+
+        switch (opcode) {
+            case INPUT:
+                a0 = program[pc + 1];
+                program[a0] = input[input_index];
+                input_index++;
+                pc += 2;
+                break;
+
+            case OUTPUT:
+                a0 = program[pc + 1];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                result = operand(program, a0, mode0);
+                output.push(result);
+                pc += 2;
+                break;
+
+            case ADD:
+            case MULT:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                a2 = program[pc + 3];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (opcode === ADD)
+                    program[a2] = operand(program, a0, mode0) + operand(program, a1, mode1);
+                else
+                    program[a2] = operand(program, a0, mode0) * operand(program, a1, mode1);
+                pc += 4;
+                break;
+
+            case JUMP_IF_TRUE:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) !== 0)
+                    pc = operand(program, a1, mode1);
+                else
+                    pc += 3;
+
+                break;
+
+            case JUMP_IF_FALSE:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) === 0)
+                    pc = operand(program, a1, mode1);
+                else
+                    pc += 3;
+
+                break;
+
+            case LESS_THAN:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                a2 = program[pc + 3];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) < operand(program, a1, mode1))
+                    program[a2] = 1;
+                else 
+                    program[a2] = 0;
+                pc += 4;
+
+                break;
+
+            case EQUALS:
+                a0 = program[pc + 1];
+                a1 = program[pc + 2];
+                a2 = program[pc + 3];
+                mode0 = Math.trunc(program[pc] / 100) % 10;
+                mode1 = Math.trunc(program[pc] / 1000) % 10;
+
+                if (operand(program, a0, mode0) === operand(program, a1, mode1))
+                    program[a2] = 1;
+                else 
+                    program[a2] = 0;
+                pc += 4;
+
+                break;
+
+            default:
+                console.log(`unknown opcode ${program[pc]} @ ${pc}`);
+                pc += 1;
+                break outer;
+        }
+    }
+
+    return output;
+}
+
+function solve(input, part) {
+    let program;
+    let output;
+    let largest_output = 0;
+    let best_a, best_b, best_c, best_d, best_e;
+
+    for (let phase_a = 0; phase_a <= 4; phase_a++)
+        for (let phase_b = 0; phase_b <= 4; phase_b++) {
+            if (phase_b === phase_a)
+                continue;
+
+            for (let phase_c = 0; phase_c <= 4; phase_c++) {
+                if (phase_c === phase_a || phase_c === phase_b)
+                    continue;
+
+                for (let phase_d = 0; phase_d <= 4; phase_d++) {
+                    if (phase_d === phase_a || phase_d === phase_b
+                        || phase_d === phase_c)
+                        continue;
+
+                    for (let phase_e = 0; phase_e <= 4; phase_e++) {
+                        if (phase_e === phase_a || phase_e === phase_b
+                            || phase_e === phase_c || phase_e == phase_d)
+                            continue;
+
+                        /* Amp A */
+                        program = input[0].split(',').map(x => Number(x));
+                        output = run(program, [phase_a, 0]);
+
+                        /* Amp B */
+                        program = input[0].split(',').map(x => Number(x));
+                        output = run(program, [phase_b, output[0]]);
+
+                        /* Amp C */
+                        program = input[0].split(',').map(x => Number(x));
+                        output = run(program, [phase_c, output[0]]);
+
+                        /* Amp D */
+                        program = input[0].split(',').map(x => Number(x));
+                        output = run(program, [phase_d, output[0]]);
+
+                        /* Amp E */
+                        program = input[0].split(',').map(x => Number(x));
+                        output = run(program, [phase_e, output[0]]);
+
+                        if (output[0] > largest_output) {
+                            largest_output = output[0];
+                            best_a = phase_a;
+                            best_b = phase_b;
+                            best_c = phase_c;
+                            best_d = phase_d;
+                            best_e = phase_e;
+                        }
+                    }
+                }
+            }
+        }
+
+    console.log(`MAX OUTPUT: ${largest_output} when [${best_a}, ${best_b}, ${best_c}, ${best_d}, ${best_e}]`);
+
+    return largest_output;
+}
+
+const expected = part => part === 1 ? 398674 : 0;
+
+module.exports = {solve,expected};
