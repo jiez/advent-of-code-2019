018571abcd8b509ed9607ed08e3427dbed974945
Jie Zhang <jzhang918@gmail.com>
2019-12-13 10:58:03 -0500

    solution for Day 13 (part 2)

--- 13/solve.js
+++ 13/solve.js
@@ -59,6 +59,10 @@ outer:
                 a0 = program[pc + 1];
                 mode0 = Math.trunc(program[pc] / 100) % 10;
                 addr = address(program, a0, mode0);
+                if (input[input_index] === undefined) {
+                    program["pc"] = pc;
+                    break outer;
+                }
                 program[addr] = input[input_index];
                 input_index++;
                 pc += 2;
@@ -70,6 +74,10 @@ outer:
                 result = operand(program, a0, mode0);
                 output.push(result);
                 pc += 2;
+                if (output.length === 3) {
+                    program["pc"] = pc;
+                    break outer;
+                }
                 break;
 
             case ADD:
@@ -167,42 +175,6 @@ outer:
 
     return output;
 }
-/*
-function show_panels(panels, width, height)
-{
-    let min_x = width, max_x = 0, min_y = height, max_y = 0;
-    const WHITE = 1;
-
-    // first narrow down the ranges
-    for (i = 0; i < panels.length; i++)
-        for (j = 0; j < panels[i].length; j++)
-            if (panels[i][j] === WHITE) {
-                if (i < min_x) min_x = i;
-                if (i > max_x) max_x = i;
-                if (j < min_y) min_y = j;
-                if (j > max_y) max_y = j;
-            }
-
-    process.stdout.write("Panels\n");
-
-    for (i = min_x; i <= max_x; i++)
-        process.stdout.write("=");
-    process.stdout.write("\n");
-
-    for (j = min_y; j <= max_y; j++) {
-        for (i = min_x; i <= max_x; i++)
-            if (panels[i][j] === 1)
-                process.stdout.write("#");
-            else
-                process.stdout.write(" ");
-        process.stdout.write("\n");
-    }
-
-    for (i = min_x; i <= max_x; i++)
-        process.stdout.write("=");
-    process.stdout.write("\n");
-}
-*/
 
 const TILE_EMPTY   = 0;
 const TILE_WALL    = 1;
@@ -210,13 +182,62 @@ const TILE_BLOCK   = 2;
 const TILE_HPADDLE = 3;
 const TILE_BALL    = 4;
 
-function solve(input, part) {
-    let program = input[0].split(',').map(x => Number(x));
+function show_screen(screen)
+{
+    let max_x = screen.length;
 
+    let max_y = 0;
+    for (i = 0; i < screen.length; i++)
+        if (max_y < screen[i].length)
+            max_y = screen[i].length;
+
+    process.stdout.write("Screen\n");
+
+    for (j = 0; j < max_y; j++) {
+        for (i = 0; i < max_x; i++) {
+            let tile = screen[i][j];
+            if (i === screen["target_x"] && j === screen["target_y"]) {
+                if (tile === TILE_HPADDLE)
+                    process.stdout.write("@");
+                else if (tile === TILE_EMPTY)
+                    process.stdout.write("*");
+                else
+                    process.stdout.write("?");
+                continue;
+            }
+
+            if (tile === TILE_EMPTY || tile === undefined)
+                process.stdout.write(" ");
+            else if (tile === TILE_WALL)
+                process.stdout.write("W");
+            else if (tile === TILE_BLOCK)
+                process.stdout.write("#");
+            else if (tile === TILE_HPADDLE)
+                process.stdout.write("=");
+            else if (tile === TILE_BALL)
+                process.stdout.write("o");
+            else
+                process.stdout.write("?");
+        }
+        process.stdout.write("\n");
+    }
+
+    process.stdout.write("\n");
+}
+
+function init_screen(program) {
     program["pc"] = 0;
     program["relative_base"] = 0;
+    let tiles = [];
 
-    let tiles = run(program, []);
+    while (true) {
+        let output = run(program, []);
+        if (output.length === 0)
+            break;
+        tiles.push(output[0]);
+        tiles.push(output[1]);
+        tiles.push(output[2]);
+    }
 
     let screen = [];
     for (i = 0; i < tiles.length; i += 3) {
@@ -233,15 +254,117 @@ function solve(input, part) {
         screen[x][y] = tile;
     }
 
+    return screen;
+}
+
+function solve(input, part) {
+    let program = input[0].split(',').map(x => Number(x));
+    let screen = init_screen(program);
     let block_count = 0;
+    let ball = {x: 0, y: 0};
+    let hpad = {x: 0, y: 0};
+    let left_wall = 0;
+    let right_wall = 0;
+
     for (x = 0; x < screen.length; x++)
         for (y = 0; y < screen.length; y++)
             if (screen[x][y] === TILE_BLOCK)
                 block_count++;
+            else if (screen[x][y] === TILE_BALL) {
+                ball.x = x;
+                ball.y = y;
+            } else if (screen[x][y] === TILE_HPADDLE) {
+                hpad.x = x;
+                hpad.y = y;
+            } else if (screen[x][y] === TILE_WALL) {
+                if (right_wall < x)
+                    right_wall = x;
+            }
 
-    return block_count;
+    let width = right_wall - left_wall - 1;
+    let height = hpad.y - 1;
+
+    //show_screen(screen);
+
+    if (part === 1)
+        return block_count;
+
+    // insert coins;
+    program[0] = 2;
+    screen = init_screen(program);
+
+    let output;
+    let move;
+    let target_x; // the target ball x value
+    let final_score = false;
+    while (!final_score) {
+        // initially we just keep still and observe the direction of ball
+        if (output === undefined)
+            move = 0;
+
+        output = run(program, [move]);
+
+        let x = output[0];
+        let y = output[1];
+        let tile = output[2];
+
+        if (x === -1 && y === 0) {
+            score = tile;
+            //console.log(`Score: ${score}`);
+            if (block_count === 0)
+                final_score = true;
+        }
+
+        // a block is broken
+        if (screen[x][y] === TILE_BLOCK && tile !== TILE_BLOCK)
+            block_count--;
+
+        // update screen
+        screen[x][y] = tile;
+
+        if (tile === TILE_BALL) {
+            // calculate hpaddle target position when ball is moving down
+            if (y > ball.y) {
+                if (x < ball.x)
+                    target_x = x - (hpad.y - y - 1);
+                else
+                    target_x = x + (hpad.y - y - 1);
+
+                while (target_x <= left_wall || target_x >= right_wall) {
+                    if (target_x <= left_wall)
+                        target_x = (left_wall + 1) + (left_wall + 1 - target_x);
+                    else // target_x >= right_wall
+                        target_x = (right_wall - 1) - (target_x - right_wall + 1);
+                }
+
+            // if ball is moving up, just follow it
+            } else if (y < ball.y)
+                target_x = x;
+
+            if (hpad.x < target_x)
+                move = 1;
+            else if (hpad.x > target_x)
+                move = -1;
+            else
+                move = 0;
+
+            ball.x = x;
+            ball.y = y;
+
+            //console.log(`hpad: ${hpad.x} target: ${target_x}, move: ${move}`);
+        } else if (tile === TILE_HPADDLE) {
+            hpad.x = x;
+            hpad.y = y;
+        }
+
+        screen["target_x"] = target_x;
+        screen["target_y"] = hpad.y;
+        //show_screen(screen);
+    }
+
+    return score;
 }
 
-const expected = part => part === 1 ? 255: 0;
+const expected = part => part === 1 ? 255: 12338;
 
 module.exports = {solve,expected};
