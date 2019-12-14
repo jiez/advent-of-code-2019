98bbc5b48846bf9573d41a8e6c35d82a7c9a9151
Jie Zhang <jzhang918@gmail.com>
2019-12-14 10:50:43 -0500

    solution for Day 14 (part 2)

--- 14/solve.js
+++ 14/solve.js
@@ -1,8 +1,37 @@
 const util = require('util');
 
+function produce_one_fuel(chemicals, reactions, partial_order) {
+    // calculate the need in the partial order
+    chemicals.set("FUEL", {"products": new Set(), "need": 1});
+
+    for (let i = 0; i < partial_order.length; i++) {
+        let chemical = partial_order[i];
+        let reaction = reactions.get(chemical);
+
+        if (reaction === undefined)
+            continue;
+
+        let need = chemicals.get(chemical)["need"];
+        let n = Math.ceil(need / reaction["units"]);
+
+        reaction["sources"].forEach(source => {
+            chemicals.get(source.chemical)["need"] += source.units * n;
+        });
+
+        // if more than need, roll over the remaining to the next time
+        chemicals.get(chemical)["need"] = need - n * reaction["units"];
+    }
+
+    let ore_need = chemicals.get("ORE")["need"];
+
+    chemicals.get("ORE")["need"] = 0;
+
+    return ore_need;
+}
+
 function solve(input, part) {
     let reactions = new Map();
-    let chemicals = new Map(); // chemcial => {"products", "amount"}
+    let chemicals = new Map(); // chemcial => {"products", "need"}
     let partial_order = [];
 
     input.map(line => {
@@ -24,7 +53,7 @@ function solve(input, part) {
     reactions.forEach((value, key, map) => {
         value["sources"].forEach(source => {
             if (chemicals.get(source["chemical"]) === undefined)
-                chemicals.set(source["chemical"], {"products": new Set(), "amount": 0});
+                chemicals.set(source["chemical"], {"products": new Set(), "need": 0});
             chemicals.get(source["chemical"])["products"].add(key);
         });
     });
@@ -55,30 +84,24 @@ function solve(input, part) {
 
     //console.log(partial_order);
 
-    // calculate the amount in the partial order
-    chemicals.set("FUEL", {"products": new Set(), "amount": 1});
+    ore_need = produce_one_fuel(chemicals, reactions, partial_order);
 
-    for (let i = 0; i < partial_order.length; i++) {
-        let chemical = partial_order[i];
-        let reaction = reactions.get(chemical);
+    if (part === 1)
+        return ore_need;
 
-        if (reaction === undefined)
-            continue;
+    let num_of_ore = 1000000000000;
+    let num_of_fuel = 0;
 
-        let amount = chemicals.get(chemical)["amount"];
-        let n = Math.ceil(amount / reaction["units"]);
-
-        reaction["sources"].forEach(source => {
-            chemicals.get(source.chemical)["amount"] += source.units * n;
-        });
+    while (num_of_ore >= ore_need) {
+        num_of_fuel++;
+        num_of_ore -= ore_need;
+        ore_need = produce_one_fuel(chemicals, reactions, partial_order);
     }
 
-    //console.log(util.inspect(chemicals, {depth: 3, colors: true}));
-
-    return chemicals.get("ORE")["amount"];
+    return num_of_fuel;
 }
 
 
-const expected = part => part === 1 ? 387001 : 0;
+const expected = part => part === 1 ? 387001 : 3412429;
 
 module.exports = {solve,expected};
