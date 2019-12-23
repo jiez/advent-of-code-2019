12a4234f55b837ee03c943e07630604d10a2c010
Jie Zhang <jzhang918@gmail.com>
2019-12-22 22:28:02 -0500

    solution for Day 22 (part 2)

--- 22/solve.js
+++ 22/solve.js
@@ -3,6 +3,10 @@ function parse_input(input) {
         return num_of_cards - 1 - pos;
     };
 
+    let combine_deal_into_new_stack = function(num_of_cards, dummy, params) {
+        return [- params[0] - 1, - params[1]];
+    };
+
     let deal_with_increment = function(num_of_cards, increment, pos) {
         return pos * increment % num_of_cards;
     };
@@ -14,6 +18,10 @@ function parse_input(input) {
 
         return pos / increment;
     }
+
+    let combine_deal_with_increment = function(num_of_cards, increment, params) {
+        return [params[0] * increment % num_of_cards, params[1] * increment % num_of_cards];
+    };
 
     let cut = function(num_of_cards, index, pos) {
         let new_pos = pos - index;
@@ -37,20 +45,23 @@ function parse_input(input) {
         return prev_pos;
     };
 
+    let combine_cut = function(num_of_cards, index, params) {
+        return [params[0] - index, params[1]];
+    }
        
     let shuffles = [];
     for (line of input) {
         if (line.includes("new")) {
             //console.log("deal into new stack");
-            shuffles.push({func: deal_into_new_stack, anti_func: deal_into_new_stack});
+            shuffles.push({func: deal_into_new_stack, anti_func: deal_into_new_stack, combine_func: combine_deal_into_new_stack});
         } else if (line.includes("increment")) {
             let increment = parseInt(line.match(/-?[0-9]+/)[0]);
             //console.log(`deal with increment ${increment}`);
-            shuffles.push({func: deal_with_increment, anti_func: anti_deal_with_increment, param: increment});
+            shuffles.push({func: deal_with_increment, anti_func: anti_deal_with_increment, combine_func: combine_deal_with_increment, param: increment});
         } else if (line.includes("cut")) {
             let index = parseInt(line.match(/-?[0-9]+/)[0]);
             //console.log(`cut ${index}`);
-            shuffles.push({func: cut, anti_func: anti_cut, param: index});
+            shuffles.push({func: cut, anti_func: anti_cut, combine_func: combine_cut, param: index});
         }
     }
 
@@ -69,6 +80,21 @@ function anti_transform(shuffles, num_of_cards, pos) {
     return pos;
 }
 
+function combine_transform(shuffles, num_of_cards, initial_params) {
+    let params = initial_params;
+    for (let i = 0; i < shuffles.length; i++)
+        params = shuffles[i].combine_func(num_of_cards, shuffles[i].param, params);
+    return params;
+}
+
+function combine_two(num_of_cards, params1, params2) {
+    let a = BigInt(params1[1]);
+    let b = BigInt(params1[0]);
+    let c = BigInt(params2[1]);
+    let d = BigInt(params2[0]);
+    return [Number((b * c + d) % BigInt(num_of_cards)), Number((a * c) % BigInt(num_of_cards))];
+}
+
 function solve(input, part) {
     let shuffles = parse_input(input);
 
@@ -76,22 +102,68 @@ function solve(input, part) {
         const num_of_cards = 10007;
         let initial_pos = 2019;
         return transform(shuffles, num_of_cards, initial_pos);
+        /*
+        // another way to calculate it
+        let params = combine_transform(shuffles, num_of_cards, [0, 1]);
+        let result = (initial_pos * params[1] + params[0]) % num_of_cards;
+        if (result < 0)
+            result += num_of_cards;
+        return result;
+        */
     } else {
-        //const num_of_cards = 119315717514047;
-        //const times = 101741582076661;
-        //let end_pos = 2020;
-        const num_of_cards = 10007;
-        const times = 1;
-        let end_pos = 8191;
+        // for small data we can use the above anti_funcs to calculate it.
+        // but for large repeat times, the following method will be more
+        // efficient. 
+        const num_of_cards = 119315717514047;
+        const total_times = 101741582076661;
+        const end_pos = 2020;
+        let initial_params = combine_transform(shuffles, num_of_cards, [0, 1]);
+        let times = total_times;
+        let final_params;
 
-        let first_repeat = 0;
-        let pos = end_pos;
-        for (i = 0; i < times; i++) {
-            pos = anti_transform(shuffles, num_of_cards, pos);
+        while (times > 0) {
+            let count = 1;
+            params = initial_params;
+            while (count * 2 <= times) {
+                params = combine_two(num_of_cards, params, params);
+                //params = combine_double(num_of_cards, params);
+                count *= 2;
+            }
+
+            if (final_params === undefined)
+                final_params = params;
+            else
+                final_params = combine_two(num_of_cards, final_params, params);
+
+            // console.log(final_params);
+
+            times -= count;
         }
 
-        console.log(`card ${end_pos} as at ${pos} before ${times} times of shuffling`);
-        return pos;
+        let original_pos;
+        // naive way
+        /*
+        let i = 0;
+        while ((i * final_params[1] + final_params[0] - end_pos) % num_of_cards !== 0)
+            i++;
+        original_pos = i;
+        */
+        // mathmetical way i.e. wolframalpha way ;)
+        let a = final_params[1];
+        let b = final_params[0];
+        // find x for
+        //   a * x + b === end_pos MOD num_of_cards
+        // i.e.
+        //   a * x === c MOD num_of_cards (c = end_pos - b) 
+        // we use wolframalpha.com to solve it for now.
+        let c = end_pos - b;
+        console.log("Find the solution for this equation on www.wolframalpha.com:");
+        console.log(`${a} x = ${c} MOD ${num_of_cards}`);
+        /* solution: x congruent 1644352419829 (mod 119315717514047) */
+        /*
+        console.log(`card ${end_pos} as at ${original_pos} before ${total_times} times of shuffling`);
+        return original_pos;
+        */
     }
 }
 
